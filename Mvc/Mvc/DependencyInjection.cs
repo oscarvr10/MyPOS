91c14492f828523508eddcefd83d27818eaa7471
Oscar Vera <overa@qubeyond.com>
2021-12-19 20:35:37 -0600

    base project net 6

---
+++ Mvc/Mvc/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace Mvc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
